33ad2dbec44c3ad42e291b05c0bb9fb2d732b209
Shane Becker <veganstraightedge@gmail.com>
2020-02-27 20:18:34 -0800

    SaferYAML.for returns a hash

--- lib/safer_yaml.rb
+++ lib/safer_yaml.rb
@@ -1,4 +1,9 @@
 require 'safer_yaml/version'
 
 module SaferYAML
+  class << self
+    def for path
+      {}
+    end
+  end
 end
--- spec/safer_yaml_spec.rb
+++ spec/safer_yaml_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe SaferYAML do
       expect(described_class.name).to eq('SaferYAML')
     end
   end
+
+  describe 'for' do
+    subject = described_class.for 'test/example.yml'
+
+    it 'returns a hash' do
+      expect(subject).to be_a Hash
+    end
+  end
 end
