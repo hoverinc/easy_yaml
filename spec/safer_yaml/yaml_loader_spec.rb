f7dd49b4f4264cd8dd589f5c6119345d4c612c08
Shane Becker <veganstraightedge@gmail.com>
2020-02-27 22:31:20 -0800

    DRY specs

--- spec/safer_yaml/yaml_loader_spec.rb
+++ spec/safer_yaml/yaml_loader_spec.rb
@@ -46,24 +46,21 @@ RSpec.describe SaferYAML::YAMLLoader do
     end
 
     context 'with ERB in the YAML file' do
-      context 'with ERB allowed' do
-        let(:erb_in_yaml_example_path) { Pathname.new File.expand_path('../examples/erb.yml', __dir__) }
-        let(:ruby_version) { RUBY_VERSION }
+      let(:erb_in_yaml_example_path) { Pathname.new File.expand_path('../examples/erb.yml', __dir__) }
+      let(:ruby_version) { RUBY_VERSION }
 
+      context 'with ERB allowed' do
         it 'parses ERB inside YAML, then reads value from YAML' do
           subject = described_class.new(erb_in_yaml_example_path).to_h
           expect(subject['ruby']['version']).to eq ruby_version
         end
       end
-    end
 
-    context 'with ERB disallowed' do
-      let(:erb_in_yaml_example_path) { Pathname.new File.expand_path('../examples/erb.yml', __dir__) }
-      let(:ruby_version) { RUBY_VERSION }
-
-      it 'parses ERB inside YAML, then reads value from YAML' do
-        subject = described_class.new(erb_in_yaml_example_path, allow_erb: false).to_h
-        expect(subject['ruby']['version']).to eq '<%= RUBY_VERSION %>'
+      context 'with ERB disallowed' do
+        it 'parses ERB inside YAML, then reads value from YAML' do
+          subject = described_class.new(erb_in_yaml_example_path, allow_erb: false).to_h
+          expect(subject['ruby']['version']).to eq '<%= RUBY_VERSION %>'
+        end
       end
     end
   end
