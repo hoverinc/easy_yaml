e73ec398ff21bf0c40eced89aab2ad09b78a5d3d
Shane Becker <veganstraightedge@gmail.com>
2020-02-27 23:44:47 -0800

    Add SaferYAML.for

--- lib/safer_yaml.rb
+++ lib/safer_yaml.rb
@@ -2,4 +2,14 @@ require 'safer_yaml/version'
 require 'safer_yaml/yaml_loader'
 
 module SaferYAML
+  class << self
+    def for path, allow_aliases: true, allow_erb: true, relative_to_rails_root: true
+      loaded_yaml = SaferYAML::YAMLLoader.new path,
+                                              allow_aliases:          allow_aliases,
+                                              allow_erb:              allow_erb,
+                                              relative_to_rails_root: relative_to_rails_root
+
+      loaded_yaml.to_h
+    end
+  end
 end
--- spec/safer_yaml_spec.rb
+++ spec/safer_yaml_spec.rb
@@ -8,4 +8,28 @@ RSpec.describe SaferYAML do
       expect(described_class.name).to eq('SaferYAML')
     end
   end
+
+  describe '.for' do
+    let(:yaml_examples_dir) { '../../spec/examples' }
+
+    it 'returns a hash' do
+      subject = described_class.for("#{yaml_examples_dir}/simple.yml")
+      expect(subject).to be_a Hash
+    end
+
+    it 'accepts a relative_to_rails_root argument' do
+      subject = described_class.for("#{yaml_examples_dir}/simple.yml", relative_to_rails_root: false)
+      expect(subject).to be_a Hash
+    end
+
+    it 'accepts a allow_aliases argument' do
+      subject = described_class.for("#{yaml_examples_dir}/simple.yml", allow_aliases: false)
+      expect(subject).to be_a Hash
+    end
+
+    it 'accepts both relative_to_rails_root and allow_aliases arguments' do
+      subject = described_class.for("#{yaml_examples_dir}/simple.yml", relative_to_rails_root: false, allow_aliases: false)
+      expect(subject).to be_a Hash
+    end
+  end
 end
