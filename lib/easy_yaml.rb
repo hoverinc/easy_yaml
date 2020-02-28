af4f6813627031b95799310309a2c64d5ecb3182
Shane Becker <veganstraightedge@gmail.com>
2020-02-28 14:44:10 -0800

    Rubocop auto-correct

--- lib/easy_yaml.rb
+++ lib/easy_yaml.rb
@@ -5,9 +5,9 @@ module EasyYAML
   class << self
     def for path, allow_aliases: true, allow_erb: true, relative_to_rails_root: true
       loaded_yaml = EasyYAML::YAMLLoader.new path,
-                                              allow_aliases:          allow_aliases,
-                                              allow_erb:              allow_erb,
-                                              relative_to_rails_root: relative_to_rails_root
+                                             allow_aliases:          allow_aliases,
+                                             allow_erb:              allow_erb,
+                                             relative_to_rails_root: relative_to_rails_root
 
       loaded_yaml.to_h
     end
