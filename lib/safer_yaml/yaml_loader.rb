5c8559b209a37bb7699a7bfb3d934c61db6dfd1b
Shane Becker <veganstraightedge@gmail.com>
2020-02-27 22:50:32 -0800

    Move Pathname-ing out of .new

--- lib/safer_yaml/yaml_loader.rb
+++ lib/safer_yaml/yaml_loader.rb
@@ -4,7 +4,7 @@ require 'erb'
 module SaferYAML
   class YAMLLoader
     def initialize path, relative_to_rails_root: true, allow_aliases: true, allow_erb: true
-      @path                   = Pathname.new File.expand_path(path, __dir__)
+      @path                   = path
       @relative_to_rails_root = relative_to_rails_root
       @allow_aliases          = allow_aliases
       @allow_erb              = allow_erb
@@ -16,8 +16,12 @@ module SaferYAML
 
     private
 
+    def file_path
+      Pathname.new File.expand_path(@path, __dir__)
+    end
+
     def yaml_file
-      File.read(@path)
+      File.read(file_path)
     end
 
     def erb_parsed_yaml
