8e0fb4b73fc3ce97f071f681733cc242cfa1b22a
Shane Becker <veganstraightedge@gmail.com>
2020-02-27 21:20:35 -0800

    Remove SaferYAML.for for now

--- lib/safer_yaml.rb
+++ lib/safer_yaml.rb
@@ -1,9 +1,4 @@
 require 'safer_yaml/version'
 
 module SaferYAML
-  class << self
-    def for path
-      {}
-    end
-  end
 end
--- spec/safer_yaml_spec.rb
+++ spec/safer_yaml_spec.rb
@@ -8,12 +8,4 @@ RSpec.describe SaferYAML do
       expect(described_class.name).to eq('SaferYAML')
     end
   end
-
-  describe 'for' do
-    subject = described_class.for 'test/example.yml'
-
-    it 'returns a hash' do
-      expect(subject).to be_a Hash
-    end
-  end
 end
